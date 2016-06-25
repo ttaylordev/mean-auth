55c0d3296bf9307df8bd82c79419bc89de48a561
Tyson <mellowt@gmail.com>
2016-06-24 19:48:59 -0600

    fixed the vars in .jade documents, added and utilized bcrypt, sessions, etc.

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-var express = require('express');
+var bcrypt = require('bcryptjs');
 var bodyParser = require('body-parser');
+var csrf = require('csurf');
+var express = require('express');
 // ORM for mongodb
 var mongoose = require('mongoose');
+var sessions = require('client-sessions');
+
 
 var Schema = mongoose.Schema;
 var ObjectId = Schema.ObjectId;
@@ -27,10 +31,45 @@ mongoose.connect('mongodb://localhost/newAuth');
 
 //middleware It's going to run each request through this function first, and then will call our function. It will take the request, and make it availiable on req.body and allow us access to it. like in the app.post...
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(sessions({
+    cookieName: 'session',
+    secret: '13298cnqiop8hiuopayd98poq23fq36y7cft7uasdi',
+    duration: 30 * 60 * 1000,
+    activeDuration: 5 * 60 * 1000
+}));
 
+app.use(csrf());
+
+app.use(function (req, res, next) {
+    'use strict';
+    if (req.session && req.session.user) {
+        User.findOne({ email: req.session.user.email}, function (err, user) {
+            if (user) {
+                req.user = user;
+                delete req.user.password;
+                req.session.user = req.user;
+                res.locals.user = req.user;
+            }
+            next();
+        });
+    } else {
+        next();
+    }
+});
+
+function requireLogin(req, res, next) {
+    'use strict';
+    if (!req.user) {
+        res.redirect('/login');
+    } else {
+        next();
+    }
+}
+
+// api
 app.get('/', function (req, res) {
     'use strict';
-    res.render('index.jade');
+    res.render('index.jade', { csrfToken: req.csrfToken()});
 });
 
 app.get('/register', function (req, res) {
@@ -38,23 +77,58 @@ app.get('/register', function (req, res) {
     res.render('register.jade');
 });
 
+
 app.post('/register', function (req, res) {
     'use strict';
-    res.json(req.body);
+    var hash = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
+    var user = new User({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: hash
+    });
+    user.save(function (err) {
+        if (err) {
+            err = 'Something bad happened.';
+            if (err.code === 11000) {
+                error = 'That email is already taken';
+            }
+            res.render('register.jade', {error: error});
+        } else {
+            res.redirect('dashboard');
+        }
+    });
 });
+
 
 app.get('/login', function (req, res) {
     'use strict';
     res.render('login.jade');
 });
 
-app.get('/dashboard', function (req, res) {
+app.post('/login', function (req, res) {
+    'use strict';
+    User.findOne({email: req.body.email}, function (err, user) {
+        if (!user) {
+            res.render('login.jade', {error: 'Invalid email or pqassword.'});
+        } else {
+            if (bcrypt.compareSync(req.body.password, user.password)) {
+                req.session.user = user;
+                res.redirect('dashboard');
+            } else { res.render('login.jade', {error: 'Invalid email or password.'});
+                }
+        }
+    });
+});
+
+app.get('/dashboard', requireLogin, function (req, res) {
     'use strict';
     res.render('dashboard.jade');
 });
 
 app.get('/logout', function (req, res) {
     'use strict';
+    req.session.reset();
     res.redirect('/');
 });
 
